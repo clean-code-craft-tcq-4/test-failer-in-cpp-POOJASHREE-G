d93c4c9df05e297d8163dd67d52627d45cee9947
POOJASHREE-G <105204354+POOJASHREE-G@users.noreply.github.com>
2022-06-20 17:06:18 +0530

    Update alerter.cpp

--- alerter.cpp
+++ alerter.cpp
@@ -25,7 +25,7 @@ int main() {
     alertInCelcius(400.5);
     alertInCelcius(303.6);
     assert(alert::networkAlertStub(farenheitTOcelcius(400.5)) == 500); // Since the threshold is 200 (in celcius), i am passing 204(in celicus) and expecting 500 as a return value.
-    assert(alertFailureCount == 1); // Number of failures doesn't match the count. Its one of the bug.
+    assert(alertFailureCount == 1); // Number of failures doesn't match the count. It's a bug.
     std::cout << alertFailureCount << " alerts failed.\n";
     std::cout << "All is well (maybe!)\n";
     return 0;
