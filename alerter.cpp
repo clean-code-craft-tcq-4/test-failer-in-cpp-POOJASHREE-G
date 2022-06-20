d1c741c3a8b3be1d4dcd391e148e18969ec0d88e
POOJASHREE-G <105204354+POOJASHREE-G@users.noreply.github.com>
2022-06-20 13:53:34 +0530

    Update alerter.cpp

--- alerter.cpp
+++ alerter.cpp
@@ -17,6 +17,7 @@ int networkAlertStub(float celcius) {
 
 void alertInCelcius(float farenheit) {
     float celcius = (farenheit - 32) * 5 / 9;
+    std::cout << "celcius = " << celcius <<"\n";
     int returnCode = networkAlertStub(celcius);
     if (returnCode != 200) {
         // non-ok response is not an error! Issues happen in life!
