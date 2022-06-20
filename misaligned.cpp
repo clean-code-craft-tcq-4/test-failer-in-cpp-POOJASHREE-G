2a7a4c11f9e163ac1bb3283755b23b81402b0ee0
POOJASHREE-G <105204354+POOJASHREE-G@users.noreply.github.com>
2022-06-20 13:16:30 +0530

    Update misaligned.cpp

--- misaligned.cpp
+++ misaligned.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 #include <assert.h>
 
+enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+
+const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
+const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+
 int printColorMap() {
-    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
-    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+
     int i = 0, j = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
@@ -13,9 +18,23 @@ int printColorMap() {
     return i * j;
 }
 
+int GetPairNumberFromColor(MajorColor major, MinorColor minor) {
+    return major * 5 + minor;
+
+}
+void testPairToNumber(MajorColor major, MinorColor minor, int expectedPairNumber)
+{
+    int pairNumber = GetPairNumberFromColor(major, minor);
+    std::cout << "Got pair number " << pairNumber << std::endl;
+    assert(pairNumber == expectedPairNumber);
+}
+
 int main() {
     int result = printColorMap();
     assert(result == 25);
+    testPairToNumber(BLACK, ORANGE, 12);
+    testPairToNumber(VIOLET, SLATE, 25);
+
     std::cout << "All is well (maybe!)\n";
     return 0;
 }
