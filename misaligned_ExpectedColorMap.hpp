772230947082f7523cddbb516afb3f1e47a12d52
POOJASHREE-G <105204354+POOJASHREE-G@users.noreply.github.com>
2022-06-27 19:02:06 +0530

    Update misaligned_ExpectedColorMap.hpp

--- misaligned_ExpectedColorMap.hpp
+++ misaligned_ExpectedColorMap.hpp
@@ -3,9 +3,6 @@
 
 #include <sstream>
 
-extern enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
-extern enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
-
 extern enum MajorColor;
 extern enum MinorColor;
 
