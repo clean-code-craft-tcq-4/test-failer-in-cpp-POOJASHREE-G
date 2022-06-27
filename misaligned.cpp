b3aa777e4e915f6bba50246cab14e61c8d169043
POOJASHREE-G <105204354+POOJASHREE-G@users.noreply.github.com>
2022-06-27 19:08:54 +0530

    Update misaligned.cpp

--- misaligned.cpp
+++ misaligned.cpp
@@ -17,7 +17,11 @@ int printColorMap() {
     int i = 0, j = 0, n = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-            buffer1[n] << i * 5 + j << " | " << majorColor[i] << " | " << minorColor[i] << "\n";
+            buffer1[n]
+                    << left << setw(10) << (i * 5 + j)+ 1 << "|\t"
+                    << left << setw(10) << majorColor[i] << "|\t"
+                    << left << setw(10) << minorColor[i]
+                    << std::endl;
             ++n;
         }
     }
